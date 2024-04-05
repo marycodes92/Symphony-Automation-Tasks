843c05fe06c7948135ef558081651a88ad5b2ddd
Mary <mary@moralis.io>
2024-04-05 13:51:52 +0100

    added ci/cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
   grep: testPlanFilter(),
-  reporter: [["html"], ["allure-playwright"], ]
+  reporter: process.env.CI ? 'dot' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
